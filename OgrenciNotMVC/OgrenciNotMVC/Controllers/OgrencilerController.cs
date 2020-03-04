97c48d6befc78152469b9b7ae7d882db3c62a5a0
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-04 15:47:25 +0300

    cs

--- OgrenciNotMVC/OgrenciNotMVC/Controllers/OgrencilerController.cs
+++ OgrenciNotMVC/OgrenciNotMVC/Controllers/OgrencilerController.cs
@@ -14,5 +14,32 @@ namespace OgrenciNotMVC.Controllers
         {
             return View(db.TBLOGRENCILER.ToList());
         }
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKULUPLER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KULUPAD,
+                                                 Value = i.KULUPID.ToString()
+                                             }
+                                             ).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniOgrenci(TBLOGRENCILER p3)
+        {
+            db.TBLOGRENCILER.Add(p3);
+            db.SaveChanges();
+            return View();
+        }
+        //List<SelectListItem> items = new List<SelectListItem>();
+        //items.Add(new SelectListItem { Text = "Matematik", Value = "0" });
+        //    items.Add(new SelectListItem { Text = "Fen Bilgisi", Value = "1" });
+        //    items.Add(new SelectListItem { Text = "Atatürk İlke ve İnkilapları", Value = "2"});
+        //    items.Add(new SelectListItem { Text = "Coğrafya", Value = "3" });
+        //    items.Add(new SelectListItem { Text = "Edebiyat", Value = "4" });
+        //    ViewBag.DersAd = items;
     }
 }
