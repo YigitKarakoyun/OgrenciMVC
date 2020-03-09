00aaea0fbde7e7c7f3bf2ebcb5a2883957f490d2
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-09 18:22:04 +0300

    cs

--- OgrenciNotMVC/OgrenciNotMVC/Controllers/DefaultController.cs
+++ OgrenciNotMVC/OgrenciNotMVC/Controllers/DefaultController.cs
@@ -38,5 +38,19 @@ namespace OgrenciNotMVC.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult DersGetir(int id)
+        {
+            var ders = db.TBLDERSLER.Find(id);
+            return View("DersGetir",ders);
+        }
+
+        public ActionResult Guncelle(TBLDERSLER p)
+        {
+            var ders = db.TBLDERSLER.Find(p.DERSID);
+            ders.DERSAD = p.DERSAD;
+            db.SaveChanges();
+            return RedirectToAction("Index","Default");
+        }
+
     }
 }
